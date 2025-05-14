4e01ddf96cffd2e964382f511faeb3e9f3286b1e
강명균 <37652751+audrbsdl@users.noreply.github.com>
2025-05-14 20:45:49 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int num_1, num_2;
+
+	cin >> num_1 >> num_2;
+	cout << num_1 * (num_2 % 10) << endl;
+	cout << num_1 * ((num_2 / 10) % 10) << endl;
+	cout << num_1 * ((num_2 / 100) % 10) << endl;
+	cout << num_1 * num_2;
+
+	return 0;
+}

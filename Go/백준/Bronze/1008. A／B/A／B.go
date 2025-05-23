d86c986025259643cbd68b193a9aa8f11000f841
강명균 <37652751+audrbsdl@users.noreply.github.com>
2025-05-24 05:18:59 +0900

    Delete Go/백준/Bronze directory

--- Go/백준/Bronze/1008. A／B/A／B.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var a, b float64
-	fmt.Scanf("%f %f", &a, &b)
-	fmt.Println(a / b)
-}
--- Go/백준/Bronze/10926. ？？！/？？！.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var str string
-	fmt.Scanf("%s", &str)
-	fmt.Println(str + "??!")
-}
